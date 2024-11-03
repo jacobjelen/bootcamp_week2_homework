67246d95f9a8b96cc19e4b8c87e3ba6264a0910f
jacobjelen <jacobjelen@gmail.com>
2024-11-03 12:49:20 +0100

    deploy script for Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,12 @@
 import { task, type HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+import * as dotenv from "dotenv";
+dotenv.config();  //loads .env variables into process.env
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
+
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.viem.getWalletClients();
   for (const account of accounts) {
@@ -10,6 +16,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  // defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      // url: "https://ethereum-sepolia-rpc.publicnode.com",
+      url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    }
+  },
 };
 
 export default config;
